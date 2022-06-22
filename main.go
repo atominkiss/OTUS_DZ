c073797aff1679524986ee1763a50b4a19650057
Anton Kuznetsov <ankuznetsov@mtt.ru>
2022-06-22 15:30:32 +0300

    DZ-1 Module testing

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-func main() {
-	solve(1, 0, 1)
-	solve(1, 0, -1)
+import "math"
 
-	solve(2, 2, 1)
-	solve(0, 1, 2)
-	//solve(math.NaN(), math.NaN(), math.NaN())
+func main() {
+	solve(1, 0, 1, 0.001)  // D < 0
+	solve(1, 0, -1, 0.001) // D > 0
+	solve(-0.2, -0.05, 0.1, 0.08250000000000005)
+	//solve(0, 1, 2, 0.001)
+	solve(math.NaN(), math.NaN(), math.NaN(), 0.001)
+	solve(math.Inf(1), math.Inf(1), math.Inf(1), 0.001)
+	solve(math.Inf(-1), math.Inf(-1), math.Inf(-1), 0.001)
 }
--- solve_test.go
+++ solve_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"math"
-	"math/big"
 	"reflect"
 	"testing"
 )
@@ -10,9 +9,10 @@ import (
 func TestSolve(t *testing.T) {
 
 	type args struct {
-		a float64
-		b float64
-		c float64
+		a   float64
+		b   float64
+		c   float64
+		eps float64
 	}
 
 	tests := []struct {
@@ -21,15 +21,17 @@ func TestSolve(t *testing.T) {
 		a       float64
 		b       float64
 		c       float64
+		eps     float64
 		want    []float64
 		wantErr string
 	}{
 		{
 			name: "no root",
 			args: args{
-				a: 1,
-				b: 0,
-				c: 1,
+				a:   1,
+				b:   0,
+				c:   1,
+				eps: 0.01,
 			},
 			want:    []float64{},
 			wantErr: "Дискриминант меньше 0!",
@@ -37,9 +39,10 @@ func TestSolve(t *testing.T) {
 		{
 			name: "2 root ",
 			args: args{
-				a: 1,
-				b: 0,
-				c: -1,
+				a:   1,
+				b:   0,
+				c:   -1,
+				eps: 0.01,
 			},
 			want:    []float64{1, -1},
 			wantErr: "",
@@ -47,19 +50,21 @@ func TestSolve(t *testing.T) {
 		{
 			name: "1 root ",
 			args: args{
-				a: 1,
-				b: 2,
-				c: 1,
+				a:   -0.2,                //1,
+				b:   -0.05,               //2,
+				c:   0.1,                 //1,
+				eps: 0.08250000000000005, //0.01,
 			},
-			want:    []float64{-1, -1},
+			want:    []float64{-0.8430703308172536, -0.8430703308172536},
 			wantErr: "",
 		},
 		{
 			name: "a == 0",
 			args: args{
-				a: 0,
-				b: 1,
-				c: 1,
+				a:   0,
+				b:   1,
+				c:   1,
+				eps: 0.01,
 			},
 			want:    []float64{},
 			wantErr: "a == 0!",
@@ -67,27 +72,46 @@ func TestSolve(t *testing.T) {
 		{
 			name: "NaN",
 			args: args{
-				a: math.NaN(),
-				b: math.NaN(),
-				c: math.NaN(),
+				a:   math.NaN(),
+				b:   math.NaN(),
+				c:   math.NaN(),
+				eps: 0.01,
 			},
 			wantErr: "Коэффициенты не числа!",
+		},
+		{
+			name: "+Inf",
+			args: args{
+				a:   math.Inf(1),
+				b:   math.Inf(1),
+				c:   math.Inf(1),
+				eps: 0.01,
+			},
+			wantErr: "Коэффициенты положительные бесконечности!",
+		},
+		{
+			name: "-Inf",
+			args: args{
+				a:   math.Inf(-1),
+				b:   math.Inf(-1),
+				c:   math.Inf(-1),
+				eps: 0.01,
+			},
+			wantErr: "Коэффициенты отрицательные бесконечности!",
 		},
 	}
 
 	for _, tt := range tests {
+
 		t.Run(tt.name, func(t *testing.T) {
-			if !math.IsNaN(tt.args.a) && !math.IsNaN(tt.args.b) && !math.IsNaN(tt.args.c) {
+			if (!math.IsNaN(tt.args.a) && !math.IsNaN(tt.args.b) && !math.IsNaN(tt.args.c)) && (!math.IsInf(tt.args.a, 1) && !math.IsInf(tt.args.b, 1) && !math.IsInf(tt.args.c, 1)) && (!math.IsInf(tt.args.a, -1) && !math.IsInf(tt.args.b, -1) && !math.IsInf(tt.args.c, -1)) {
 
-				if big.NewFloat(tt.args.a).Cmp(big.NewFloat(0)) != 0 {
+				result, err := solve(tt.args.a, tt.args.b, tt.args.c, tt.args.eps)
 
-					result, err := solve(tt.args.a, tt.args.b, tt.args.c)
-
-					if err.Error() != tt.wantErr {
-						t.Errorf("have = %v, want %v", err, tt.wantErr)
-					} else if !reflect.DeepEqual(result, tt.want) {
-						t.Errorf("have = %v, want %v", result, tt.want)
-					}
+				if err.Error() != tt.wantErr {
+					t.Errorf("have = %v, want %v", err, tt.wantErr)
+				} else if !reflect.DeepEqual(result, tt.want) {
+					t.Errorf("have = %v, want %v", result, tt.want)
 				}
 			}
 		})
